d79e62f0bbd5050712714d10d745659d4b19f12c
“SHENEL98” <wsakurera@gmail.com>
2023-03-30 17:01:02 +0200

    AttributBinding -> v-bind:href,  :href

---
+++ AttributBinding/app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+     data(){
+        return {
+            showBooks: true,
+            url :'https://www.w3schools.com/',
+            //data passing by array
+            books:[
+                {title: 'title 1', author:'author 1'},
+                {title: 'title 2', author:'author 2'},
+                {title: 'title 3', author:'author 3'},
+            ]
+        }
+     },
+     methods: {
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e){
+            console.log(e, e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+     },
+     
+})
+
+app.mount('#app')
