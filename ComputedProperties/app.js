976a87ce482e4a42a074afdd555dbd4b50b75aa7
“SHENEL98” <wsakurera@gmail.com>
2023-03-30 21:22:35 +0200

    Computed Properties

---
+++ ComputedProperties/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+     data(){
+        return {
+            showBooks: true,
+            url :'https://www.w3schools.com/',
+            //data passing by array
+            books:[
+                {title: 'title 1', author:'author 1' ,img:'assets/1.jpg', isFav:true},
+                {title: 'title 2', author:'author 2' ,img:'assets/2.jpg', isFav:false},
+                {title: 'title 3', author:'author 3' ,img:'assets/3.jpg', isFav:true},
+            ]
+        }
+     },
+     methods: {
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+     },
+     computed:{
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
+        }
+     }
+     
+})
+
+app.mount('#app')
