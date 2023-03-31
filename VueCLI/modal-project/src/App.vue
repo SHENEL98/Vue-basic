26a3efa2fec8106ab51b354728f9accac31053a7
“SHENEL98” <wsakurera@gmail.com>
2023-03-31 15:50:46 +0200

    Vue CLI : files and template

---
+++ VueCLI/modal-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>My First Vue App </h2>
+    <h3>{{ title }}</h3>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue App :)",
+    };
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
