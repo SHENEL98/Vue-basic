6b4587d06904037e06602443a3ee3af2c8fd9ded
“SHENEL98” <wsakurera@gmail.com>
2023-04-01 13:18:48 +0200

    Reaction timer game : project setup

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay"/>
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+
+export default {
+  name: "App",
+  components: {
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null, //time toappear block
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000, //mini secodns
+      this.isPlaying = true,
+      console.log(this.delay)
+
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
