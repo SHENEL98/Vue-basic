54e9ec5ca340af66a2a0b2b4e100661e9ce9a5b1
“SHENEL98” <wsakurera@gmail.com>
2023-04-04 09:13:21 +0200

    Template refs

---
+++ composition_api/src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+   <h2>Home</h2>
+   <p ref="p">Name is : {{ name }} and age {{ age }}</p>
+
+   <button @click="handleClick">CLick Me</button>
+
+   </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { ref } from 'vue' 
+
+export default {
+  name: 'HomeView',
+  setup(){
+    console.log('setup')
+
+    const p = ref(null)
+
+    let name = 'roy'
+    let age =30
+    //these are not reactive values(above values)
+
+    const handleClick = () => {
+      console.log('you clicked me') 
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello, How are you ?' //in here when you click button ref(p) will change to this content
+    }
+
+    return{
+      name:name,
+      age, // this is same as above
+      handleClick,
+      p
+    }
+  },
+  // data(){
+  //   return{
+  //     //this are reactive values
+  //     age: 40
+  //   }
+  // }
+
+  // created(){
+  //   console.log('created')
+  // },
+  // mounted(){
+  //   console.log('mounted')
+  // }
+}
+</script>
