7bda905cbf360a902e9e94599ca7af9cca1108d2
“SHENEL98” <wsakurera@gmail.com>
2023-04-04 10:22:43 +0200

    Watch and WatchEffect

--- composition_api/src/views/HomeView.vue
+++ composition_api/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
+    <input type="text" v-model="search" />
     <p>All names</p>
     <div v-for="name in names" :key="name">
       {{ name }}
@@ -12,30 +12,50 @@
     <div v-for="name in matchingNames" :key="name">
       {{ name }}
     </div>
+
+    <button @click="handleClick">Stop watch</button>
   </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    // const name = computed(() => {
-    //   return 'KAMAL'
+    const search = ref("");
+    const names = ref(["name1", "name2", "roy", "nizar", "marco"]);
+
+    // watch(search,() =>{
+    //   console.log('watch function ran')
     // })
 
-    const search = ref('')
-    const names = ref(['name1', 'name2','roy','nizar','marco'])
+    // watchEffect(() =>{
+    //   console.log('watchEffect function ran',search.value)
+    // })
 
-    const  matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
+    //stop watching
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+    });
 
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value);
+    });
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    const handleClick = () =>{
+      stopWatch()
+      stopEffect()
+    }
     return {
-      names, search,matchingNames
+      names,
+      search,
+      matchingNames,handleClick
     };
   },
-  
 };
 </script>
